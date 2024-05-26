3dc4007f91ce97b8c222218509a8fa77e422659c
Jana Bollineni <jana@neni-x3.neni.io>
2024-05-26 15:07:57 -0400

    updates

---
+++ snapclient/run.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/with-contenv bashio
+
+CONFIG_PATH=/data/options.json
+
+SNAPCLIENT_OPTS=$(jq --raw-output ".snapclientopts" $CONFIG_PATH)
+
+echo "Start Snapclient..."
+/usr/bin/snapclient ${SNAPCLIENT_OPTS}
