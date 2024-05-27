1a80da67806c3d477995a69b99ff96ec41188a7c
Jana Bollineni <jana@neni-x3.neni.io>
2024-05-27 09:48:15 -0400

    updates

--- librespot/run.sh
+++ librespot/run.sh
@@ -2,9 +2,17 @@
 
 CONFIG_PATH=/data/options.json
 
-LIBRESPOT_OPTS=$(jq --raw-output ".librespotopts" $CONFIG_PATH)
+LIBRESPOT_NAME="$(bashio::config 'LIBRESPOT_NAME')"
+LIBRESPOT_USER="$(bashio::config 'LIBRESPOT_NAME')"
+LIBRESPOT_PASSWORD="$(bashio::config 'LIBRESPOT_NAME')"
+LIBRESPOT_INITVOL="$(bashio::config 'LIBRESPOT_NAME')"
+LIBRESPOT_DEVICE="$(bashio::config 'LIBRESPOT_NAME')"
+LIBRESPOT_BITRATE="$(bashio::config 'LIBRESPOT_NAME')"
+LIBRESPOT_DEVICETYPE="$(bashio::config 'LIBRESPOT_NAME')"
+LIBRESPOT_BACKEND="$(bashio::config 'LIBRESPOT_NAME')"
 
-mkdir -p /share/snapfifo
+mkdir -p /share/snapcast/
+mkfifo /share/snapcast/fifo
 
 echo "Start Librespot..."
-librespot ${LIBRESPOT_OPTS}
+librespot -n "$LIBRESPOT_NAME" -u "$LIBRESPOT_USER" -p "$LIBRESPOT_PASSWORD" --device "$LIBRESPOT_DEVICE" --backend "$LIBRESPOT_BACKEND" -b "$LIBRESPOT_BITRATE" --initial-volume "$LIBRESPOT_INITVOL" --device-type "$LIBRESPOT_DEVICETYPE"
